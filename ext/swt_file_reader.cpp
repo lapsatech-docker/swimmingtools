8a0960e1eaadf93b3f7ca88868ad64b4551ae8ac
swimmingwatchtools <swimmingwatchtools@gmail.com>
2015-01-19 16:42:29 -0500

    UPDATE: Support versions asiatique de la FR910

--- ext/swt_file_reader.cpp
+++ ext/swt_file_reader.cpp
@@ -46,7 +46,10 @@ void swt::FileReader::OnMesg(fit::Mesg& mesg) {
       swim_file.reset(new GarminSwimFile());
       swim_file->AddMesg(&mesg);
     } else if (fileId.GetManufacturer() == FIT_MANUFACTURER_GARMIN 
-        && fileId.GetProduct() == FIT_GARMIN_PRODUCT_FR910XT) {
+        && ((fileId.GetProduct() == FIT_GARMIN_PRODUCT_FR910XT) ||
+          (fileId.GetProduct() == FIT_GARMIN_PRODUCT_FR910XT_CHINA) ||
+          (fileId.GetProduct() == FIT_GARMIN_PRODUCT_FR910XT_JAPAN) ||
+          (fileId.GetProduct() == FIT_GARMIN_PRODUCT_FR910XT_KOREA))) {
       swim_file.reset(new Fr910SwimFile());
       swim_file->AddMesg(&mesg);
     } else if (fileId.GetManufacturer() == FIT_MANUFACTURER_GARMIN 
