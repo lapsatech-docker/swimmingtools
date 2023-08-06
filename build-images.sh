1deb5cc2187a0470b7ac999be32eb48c9260ccb8
Vadim Isaev <vadim.o.isaev@gmail.com>
2023-08-06 16:29:54 +0100

    shorter image name

--- build-images.sh
+++ build-images.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 true \
-   && docker build --progress=plain --no-cache -t lapsatech/swimmingtools-php-fpm:latest ./src/docker-php-fpm \
-   && docker build --progress=plain --no-cache -t lapsatech/swimmingtools-nginx:latest   ./src/docker-nginx \
-   && docker push lapsatech/swimmingtools-php-fpm:latest \
-   && docker push lapsatech/swimmingtools-nginx:latest \
+   && docker build --progress=plain --no-cache -t lapsatech/swt-php-fpm:latest ./src/docker-php-fpm \
+   && docker build --progress=plain --no-cache -t lapsatech/swt-nginx:latest   ./src/docker-nginx \
+   && docker push lapsatech/swt-php-fpm:latest \
+   && docker push lapsatech/swt-nginx:latest \
    && true
